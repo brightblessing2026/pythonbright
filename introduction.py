0f55675d3df4fbe9220971f37c75df1d8fa262bd
brightblessing2026 <brightblessing2026@gmail.com>
2026-02-03 10:34:03 +0300

    initial commit

---
+++ introduction.py
@@ -0,0 +1,7 @@
+#print is a method used to display an output
+
+
+print("Welcome to Python Programming")
+print("Bright")
+print(100)
+print(20*3)
---
+++ variables.py
@@ -0,0 +1,39 @@
+#variable is a container used to store a value
+
+
+school = "eMobilis"
+firstname = "Bright"
+lastname = "Blessing"
+num1 = 37
+num2 = 43
+language = "Python"
+
+print(school)
+print(firstname,lastname)
+print( num1 + num2)
+print(language,"is a web programming language")
+
+
+
+print("the sum of",num1 ,"amd", num2 ,"is", num1 + num2 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
